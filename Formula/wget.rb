2d76d9a5f2d4f2bdf5ca4657dfb0cdf611aef2dd
implicated <implicated.cc@gmail.com>
2026-01-28 18:07:58 +0800

    test homebrew-tap

---
+++ Formula/wget.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Wget < Formula
+  desc "test homebrew-tap"
+  homepage ""
+  url "https://mirror.ibcp.fr/pub/gnu/wget/wget-1.25.0.tar.gz"
+  sha256 "766e48423e79359ea31e41db9e5c289675947a7fcf2efdcedb726ac9d0da3784"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test wget`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ qutebrowser.rb
@@ -0,0 +1,34 @@
+cask "qutebrowser" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "3.6.3"
+  sha256 arm:   "2a4fae5acd90268b9a0fd53999adc026c4781eda96fc1961f3333d15a8f5bb4d",
+         intel: "0afb91af650548a6f52ff94e6875001fb72d94e0c5fb8d0afe4fa0a099b31dc8"
+
+  url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}-#{arch}.dmg",
+      verified: "github.com/qutebrowser/qutebrowser/"
+  name "qutebrowser"
+  desc "Keyboard-driven, vim-like browser based on PyQt5"
+  homepage "https://www.qutebrowser.org/"
+
+  depends_on macos: ">= :big_sur"
+
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/qutebrowser.wrapper.sh"
+  app "qutebrowser.app"
+  binary shimscript, target: "qutebrowser"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      '#{appdir}/qutebrowser.app/Contents/MacOS/qutebrowser' "$@"
+    EOS
+  end
+
+  zap trash: [
+        "~/Library/Application Support/qutebrowser",
+        "~/Library/Caches/qutebrowser",
+        "~/Library/Preferences/qutebrowser",
+      ],
+      rmdir: "~/.qutebrowser"
+end
